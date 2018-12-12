bbf383329a3500b3afc6b517972150b8a727d459
mewhu <mew31716@gmail.com>
2018-12-12 15:32:19 +0800

    add login process and update application.html.erb(view) with login message

--- app/controllers/endgrades_controller.rb
+++ app/controllers/endgrades_controller.rb
@@ -1,4 +1,8 @@
 class EndgradesController < ApplicationController
+  # devise認證
+  # [PS] 要改登入頁面的畫請改這個 app/views/devise/sessions/new.html.erb
+  before_action :authenticate_user!
+
   def index
   end
 end
