0d0ea7587308e3a774eadf6f7b19830ee280c295
mewhu <mew31716@gmail.com>
2018-12-12 14:53:41 +0800

    1) bundle install | 2) rails generate devise:install | 3) rails generate devise:views | 4) rails generate devise user | 5) edit the migration file based on "4)" | 6) rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181211085010) do
+ActiveRecord::Schema.define(version: 20181212064726) do
 
   create_table "attributes", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,19 @@ ActiveRecord::Schema.define(version: 20181211085010) do
     t.boolean "is_mission_3"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
